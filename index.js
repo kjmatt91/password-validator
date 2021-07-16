d0c8345c6fbdd697064efc481486b7d0f3cf6355
kjmatt91 <kjmatt91@gmail.com>
2021-07-16 19:47:47 -0400

    added variables and proper code to if functions to make most tests run

--- index.js
+++ index.js
@@ -2,37 +2,42 @@
 function validatePassword(password) {
   // if (password !== requirements) {
   //   return false
-  // } Do I need this if function?????
+  // }
+
+  let length = password.length >= 8
+  let lowerCase = false
+  let upperCase = false
+  let number = false
+  let specialChar = false
+  let specChars = '!@#$%^&*'
+
+
 
   for (let i = 0; i < password.length; i++) {
     let character = password[i]
 
-    // Must be a minimum of 8 characters
-    if (password.length < 8) {
-      return false
-    }
     // Must contain at least one lower case letter
-    if (lowerCase < 1) {
-      return false
+    if (character === character.toLowerCase()) {
+      lowerCase = true
     }
     // Must contain at least one upper case letter
-    if (upperCase < 1) {
-      return false
+    if (character === character.toUpperCase()) {
+      upperCase = true
     }
     // Must contain at least one numeric value
-    if (number < 1) {
-      return false
+    if (character >= '0' && character <= '9') {
+      number = true
     }
     // Must contain at least one special character
-    if (specialChar < 1) {
-      return false
+    if (specChars.indexOf(character) > -1) {
+      specialChar = true
     }
   }
 
-  return true
+  return lowerCase && upperCase && length && number && specialChar
 }
 
 
-// let requirements = lowerCase + upperCase + do i even need this???
+// let requirements = lowerCase + upperCase + do i need this???
 
 module.exports = validatePassword
