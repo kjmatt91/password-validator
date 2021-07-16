6746a4af7012fc7b5d987bbe6551c702da02e3c7
kjmatt91 <kjmatt91@gmail.com>
2021-07-16 00:06:26 -0400

    Setup basic structure of if functions

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 // Returns true when passwoerd meets all requirements
 function validatePassword(password) {
-  if (password !== requirements) {
-    return false
+  // if (password !== requirements) {
+  //   return false
+  // } Do I need this if function?????
+
+  for (let i = 0; i < password.length; i++) {
+    let character = password[i]
+
+    // Must be a minimum of 8 characters
+    if (password.length < 8) {
+      return false
+    }
+    // Must contain at least one lower case letter
+    if (lowerCase < 1) {
+      return false
+    }
+    // Must contain at least one upper case letter
+    if (upperCase < 1) {
+      return false
+    }
+    // Must contain at least one numeric value
+    if (number < 1) {
+      return false
+    }
+    // Must contain at least one special character
+    if (specialChar < 1) {
+      return false
+    }
   }
 
   return true
 }
-// Must be a minimum of 8 characters
-// Must contain at least one lower case letter
-// Must contain at least one upper case letter
-// Must contain at least one numeric value
-// Must contain at least one special character
 
-let requirements = // all required functions combined
+
+// let requirements = lowerCase + upperCase + do i even need this???
 
 module.exports = validatePassword
